62959651bd1e36b23a040a44fc1dfa43bd49672d
Joaquin Labarta <labartajoaquin@gmail.com>
2024-04-08 18:08:59 -0300

    Ej9 correfido

--- Practica1/Ej9/Main.java
+++ Practica1/Ej9/Main.java
@@ -5,10 +5,10 @@ public class Main{
         String str2 = "{({))]";
         if (TestBalanceo.esBalanceado(str)) {
             System.out.println("La palabra "+str+" esta balanceada");   
-        } else System.out.println("La palabra "+str+" esta balanceada"); 
+        } else System.out.println("La palabra "+str+" esta desbalanceada"); 
 
         if (TestBalanceo.esBalanceado(str2)) {
             System.out.println("La palabra "+str2+" esta balanceada");   
-        } else System.out.println("La palabra "+str2+" esta balanceada"); 
+        } else System.out.println("La palabra "+str2+" esta desbalanceada"); 
     }
 }
