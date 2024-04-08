400b7bdb4152c050f4c24fdf5e9f1723443ad96e
Joaquin Labarta <labartajoaquin@gmail.com>
2024-04-08 17:55:36 -0300

    Ej4  corregido

--- Practica2/Ej4/RedBinariaLlena.java
+++ Practica2/Ej4/RedBinariaLlena.java
@@ -3,21 +3,15 @@ package Practica2.Ej4;
 import Practica2.BinaryTree;
 
 public class RedBinariaLlena {
-    public static int retardoReenvio(BinaryTree<Integer> arbol){
-        if (arbol.isEmpty()) {
-            return 0;
-        }
-
-        int total = 0;
-
-        if (arbol.hasLeftChild() && arbol.hasRightChild()) {
-            if (arbol.getLeftChild().getData() > arbol.getRightChild().getData()) {
-                total += retardoReenvio(arbol.getLeftChild());
-            } else total+= retardoReenvio(arbol.getRightChild());
-        }
-
-        total += arbol.getData();
-
-        return total;
-    }
+    public static int retardoReenvio(BinaryTree<Integer> ab) {
+		if(ab!=null&&!ab.isEmpty()) {
+			/*Utilizo recorrido postOrden para llegar a las hojas y a partir
+			de ahí subo enviando el dato del nodo más el su hijo con mayor valor
+			*/
+			int izq = retardoReenvio(ab.getLeftChild());
+			int der = retardoReenvio(ab.getRightChild());
+			return (izq>der?(ab.getData()+izq):(ab.getData()+der));
+		}
+		return 0;
+	}
 }
--- Practica2/Ej5/Test.java
+++ Practica2/Ej5/Test.java
@@ -4,27 +4,17 @@ import Practica2.BinaryTree;
 
 public class Test{
     public static void main(String[] args) {
-        BinaryTree<Integer> arbol = new BinaryTree<>(1);
+        BinaryTree<Integer> arbol = new BinaryTree<>(5);
 
         // Nivel 1
-        arbol.addLeftChild(new BinaryTree<>(2));
-        arbol.addRightChild(new BinaryTree<>(3));
+        arbol.addLeftChild(new BinaryTree<>(10));
+        arbol.addRightChild(new BinaryTree<>(2));
 
         // Nivel 2
-        arbol.getLeftChild().addLeftChild(new BinaryTree<>(4));
-        arbol.getLeftChild().addRightChild(new BinaryTree<>(5));
-        arbol.getRightChild().addLeftChild(new BinaryTree<>(6));
-        arbol.getRightChild().addRightChild(new BinaryTree<>(7));
-
-        // Nivel 3
-        arbol.getLeftChild().getLeftChild().addLeftChild(new BinaryTree<>(8));
-        arbol.getLeftChild().getLeftChild().addRightChild(new BinaryTree<>(9));
-        arbol.getLeftChild().getRightChild().addLeftChild(new BinaryTree<>(10));
-        arbol.getLeftChild().getRightChild().addRightChild(new BinaryTree<>(11));
-        arbol.getRightChild().getLeftChild().addLeftChild(new BinaryTree<>(12));
-        arbol.getRightChild().getLeftChild().addRightChild(new BinaryTree<>(13));
-        arbol.getRightChild().getRightChild().addLeftChild(new BinaryTree<>(14));
-        arbol.getRightChild().getRightChild().addRightChild(new BinaryTree<>(15));
+        arbol.getLeftChild().addLeftChild(new BinaryTree<>(13));
+        arbol.getLeftChild().addRightChild(new BinaryTree<>(4));
+        arbol.getRightChild().addLeftChild(new BinaryTree<>(100));
+        arbol.getRightChild().addRightChild(new BinaryTree<>(5));
 
         arbol.porNiveles(arbol);
 
